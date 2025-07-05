940ce80989f23d5d918fb71b9715928a1af8e1cd
adagioCHAN <gguakchan@gmail.com>
2025-07-05 11:35:04 +0900

    [feat] MyProfileTitle 컴포넌트 제작

---
+++ app/src/main/java/com/konkuk/moru/presentation/myactivity/component/MyProfileTitle.kt
@@ -0,0 +1,54 @@
+package com.konkuk.moru.presentation.myactivity.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.konkuk.moru.R
+import com.konkuk.moru.ui.theme.MORUTheme.colors
+import com.konkuk.moru.ui.theme.MORUTheme.typography
+
+@Composable
+fun MyProfileTitle(modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(56.dp)
+    ) {
+        Text(
+            text = "MY PROFILE",
+            style = typography.body_SB_16,
+            color = colors.black,
+            modifier = Modifier
+        )
+
+        Icon(
+            painterResource(R.drawable.ic_gear),
+            contentDescription = "Settings Icon",
+            tint = colors.mediumGray,
+            modifier = Modifier
+                .padding(end = 12.dp)
+                .size(20.dp)
+        )
+
+    }
+}
+
+@Preview
+@Composable
+private fun MyProfileTitlePreview() {
+    MyProfileTitle()
+}
--- app/src/main/java/com/konkuk/moru/presentation/myactivity/screen/ActMainScreen.kt
+++ app/src/main/java/com/konkuk/moru/presentation/myactivity/screen/ActMainScreen.kt
@@ -1,10 +1,41 @@
 package com.konkuk.moru.presentation.myactivity.screen
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.konkuk.moru.presentation.myactivity.component.MyProfileTitle
+import com.konkuk.moru.ui.theme.MORUTheme.colors
 
 @Composable
 fun ActMainScreen(modifier: Modifier = Modifier) {
-    Text("My Activity Screen", modifier = modifier)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFFFFF))
+            .padding(start = 16.dp, end = 16.dp)
+    ) {
+        MyProfileTitle()
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+                .background(colors.lightGray)
+        ){}
+    }
+}
+
+@Preview
+@Composable
+private fun ActMainScreenPreview() {
+    ActMainScreen(modifier = Modifier)
 }
