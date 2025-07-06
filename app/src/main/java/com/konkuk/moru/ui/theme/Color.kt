a2609569af14f74b5cad15a1ef1f1a215142cea6
adagioCHAN <gguakchan@gmail.com>
2025-07-06 11:29:18 +0900

    [refactor] red 추가

--- app/src/main/java/com/konkuk/moru/ui/theme/Color.kt
+++ app/src/main/java/com/konkuk/moru/ui/theme/Color.kt
@@ -14,6 +14,7 @@ val LightGray = Color(color = 0xFFD9D9D9)
 val VeryLightGray = Color(color = 0xFFF1F3F5)
 val CharcoalBlack = Color(color = 0xFF1A1A1A)
 val Black50Oopacity = Color(color = 0x80000000)
+val red = Color(color = 0xFFED4569)
 
 @Immutable
 data class MoruColors(
@@ -26,7 +27,8 @@ data class MoruColors(
     val lightGray: Color,
     val veryLightGray: Color,
     val charcoalBlack: Color,
-    val black50Oopacity: Color
+    val black50Oopacity: Color,
+    val red: Color
 )
 
 val defaultMoruColors = MoruColors(
@@ -39,7 +41,8 @@ val defaultMoruColors = MoruColors(
     lightGray = LightGray,
     veryLightGray = VeryLightGray,
     charcoalBlack = CharcoalBlack,
-    black50Oopacity = Black50Oopacity
+    black50Oopacity = Black50Oopacity,
+    red = red
 )
 
 val LocalMoruColorsProvider = staticCompositionLocalOf { defaultMoruColors }
