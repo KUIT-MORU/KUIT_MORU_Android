9ff442aca9e469d631322c7407393beeb4f5206b
adagioCHAN <gguakchan@gmail.com>
2025-07-06 11:53:29 +0900

    [refactor] RoutinePaceInfo scroll 기능 추가

--- app/src/main/java/com/konkuk/moru/presentation/myactivity/component/RoutinePaceInfo.kt
+++ app/src/main/java/com/konkuk/moru/presentation/myactivity/component/RoutinePaceInfo.kt
@@ -6,11 +6,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -46,6 +49,7 @@ fun RoutinePaceInfo(
         Column(
             modifier = Modifier
                 .fillMaxWidth()
+                .background(Color(0xFFFFFFFF))
                 .padding(horizontal = 32.dp)
         ) {
             Spacer(modifier = Modifier.height(12.dp))
@@ -63,52 +67,63 @@ fun RoutinePaceInfo(
             Box(
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally)
-            ){
+            ) {
                 Text(
                     text = "루틴 페이스",
                     style = typography.body_SB_16
                 )
             }
             Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = "루틴 페이스는 일주일간의 실천 데이터를 바탕으로,\n당신의 루틴 실천 리듬을 분석 합니다.\n"+
-                        "매주 월요일, 지금 나의 루틴 페이스는 어떤지 알 수 있어요.",
-                style = typography.time_R_12.copy(
-                    lineHeight = 20.sp
-                ),
-                color = colors.mediumGray
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(208.dp)
-                    .background(color = colors.veryLightGray, RoundedCornerShape(12.dp))
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Row(
-                horizontalArrangement = Arrangement.End,
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("최근 갱신 일자: ", style = typography.time_R_12, color = colors.mediumGray)
-                Text(text = renewalDate, style = typography.time_R_12, color = colors.mediumGray)
-            }
-            Spacer(modifier = Modifier.height(8.dp))
-            Button(
-                onClick = onDetailClick,
-                colors = ButtonDefaults.buttonColors(containerColor = colors.limeGreen),
-                shape = RoundedCornerShape(10.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(60.dp)
-                    .padding(bottom = 10.dp)
+
+            val scrollState = rememberScrollState()
+            Column(
+                modifier = modifier
+                    .verticalScroll(scrollState)
             ) {
                 Text(
-                    text = "자세히 보기",
-                    color = colors.black,
-                    style = typography.body_SB_16,
+                    text = "루틴 페이스는 일주일간의 실천 데이터를 바탕으로,\n당신의 루틴 실천 리듬을 분석 합니다.\n" +
+                            "매주 월요일, 지금 나의 루틴 페이스는 어떤지 알 수 있어요.",
+                    style = typography.time_R_12.copy(
+                        lineHeight = 20.sp
+                    ),
+                    color = colors.mediumGray
                 )
+                Spacer(modifier = Modifier.height(16.dp))
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(208.dp)
+                        .background(color = colors.veryLightGray, RoundedCornerShape(12.dp))
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("최근 갱신 일자: ", style = typography.time_R_12, color = colors.mediumGray)
+                    Text(
+                        text = renewalDate,
+                        style = typography.time_R_12,
+                        color = colors.mediumGray
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Button(
+                    onClick = onDetailClick,
+                    colors = ButtonDefaults.buttonColors(containerColor = colors.limeGreen),
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(60.dp)
+                        .padding(bottom = 10.dp)
+                ) {
+                    Text(
+                        text = "자세히 보기",
+                        color = colors.black,
+                        style = typography.body_SB_16,
+                    )
+                }
             }
         }
     }
