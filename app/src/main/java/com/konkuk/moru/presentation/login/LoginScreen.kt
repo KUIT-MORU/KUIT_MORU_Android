5fbcaddcaed7914669639805f091bd9ea203b25f
hyungyu-02 <yona20020209@gmail.com>
2025-07-10 00:46:04 +0900

    feat: Login 화면 UI 구현

---
+++ app/src/main/java/com/konkuk/moru/presentation/login/LoginScreen.kt
@@ -0,0 +1,141 @@
+package com.konkuk.moru.presentation.login
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.konkuk.moru.presentation.login.component.LoginTextField
+import com.konkuk.moru.presentation.login.component.LoginTextFieldBasic
+import com.konkuk.moru.presentation.login.component.LogoWithTitle
+import com.konkuk.moru.presentation.navigation.Route
+import com.konkuk.moru.ui.theme.MORUTheme.colors
+import com.konkuk.moru.ui.theme.MORUTheme.typography
+
+@Composable
+fun LoginScreen(navController: NavController) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var errorMessage by remember { mutableStateOf<String?>(null) }
+
+    val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colors.charcoalBlack)
+            .padding(horizontal = 32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(screenHeight * 0.1825f))
+        LogoWithTitle()
+        Spacer(modifier = Modifier.height(32.dp))
+
+        LoginTextFieldBasic(
+            value = email,
+            onValueChange = { email = it },
+            placeholder = "이메일",
+            keyboardType = KeyboardType.Email
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        LoginTextFieldBasic(
+            value = password,
+            onValueChange = { password = it },
+            placeholder = "비밀번호",
+            keyboardType = KeyboardType.Password,
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(34.dp)
+        ) {
+            errorMessage?.let {
+                Text(
+                    text = "이메일 또는 비밀번호가 일치하지 않습니다.",
+                    style = typography.desc_M_12,
+                    color = colors.red,
+                    modifier = Modifier.padding(top = 6.dp)
+                )
+            }
+//            Text(
+//                text = "이메일 또는 비밀번호가 일치하지 않습니다.",
+//                style = typography.desc_M_12,
+//                color = colors.red,
+//                modifier = Modifier.padding(top = 6.dp)
+//            )
+        }
+
+        Button(
+            onClick = {
+                errorMessage = "이메일 또는 비밀번호가 일치하지 않습니다."
+            },
+            shape = RoundedCornerShape(4.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colors.limeGreen,
+                contentColor = Color(0xFF212120)
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(46.dp)
+        ) {
+            Text("로그인", style = typography.body_SB_16)
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        HorizontalDivider(
+            thickness = 1.dp,
+            color = colors.darkGray
+        )
+
+        Text(
+            text = "회원가입 하기",
+            style = typography.desc_M_14,
+            color = colors.mediumGray,
+            modifier = Modifier
+                .padding(top = 12.dp)
+                .clickable{
+                    navController.navigate(Route.SignUp.route)
+                }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    val navController = rememberNavController()
+    LoginScreen(navController = navController)
+}
---
+++ app/src/main/java/com/konkuk/moru/presentation/login/component/LoginTextField.kt
@@ -0,0 +1,127 @@
+package com.konkuk.moru.presentation.login.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.konkuk.moru.ui.theme.MORUTheme.colors
+import com.konkuk.moru.ui.theme.MORUTheme.typography
+
+@Composable
+fun LoginTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholder: String,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    visualTransformation: VisualTransformation = VisualTransformation.None
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = { Text(placeholder, style = typography.desc_M_14) },
+        textStyle = typography.desc_M_14,
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+        visualTransformation = visualTransformation,
+        singleLine = true,
+        shape = RoundedCornerShape(4.dp),
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = colors.limeGreen,
+            unfocusedIndicatorColor = colors.darkGray,
+            focusedContainerColor = Color.Transparent,
+            unfocusedContainerColor = Color.Transparent,
+            focusedTextColor = Color.White,
+            unfocusedTextColor = Color.White,
+            focusedPlaceholderColor = colors.lightGray,
+            unfocusedPlaceholderColor = colors.mediumGray
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(46.dp),
+    )
+}
+
+@Composable
+fun LoginTextFieldBasic(
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholder: String,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+
+    val borderColor = if (isFocused) colors.limeGreen else colors.darkGray
+
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChange,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+        visualTransformation = visualTransformation,
+        interactionSource = interactionSource,
+        textStyle = typography.desc_M_14.copy(
+            color = Color.White,
+        ),
+        cursorBrush = SolidColor(Color.White),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(46.dp)
+            .border(
+                width = 1.dp,
+                color = borderColor,
+                shape = RoundedCornerShape(4.dp)
+            )
+            .background(color = Color.Transparent),
+        decorationBox = { innerTextField ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize().padding(horizontal = 16.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                if (value.isEmpty()) {
+                    Text(
+                        text = placeholder,
+                        style = typography.desc_M_14,
+                        color = if (isFocused) colors.lightGray else colors.mediumGray,
+                    )
+                }
+                innerTextField()
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF212120)
+@Composable
+private fun BasicPreview() {
+    LoginTextFieldBasic(
+        value = "",
+        onValueChange = {},
+        placeholder = "이메일",
+        keyboardType = KeyboardType.Email,
+        visualTransformation = VisualTransformation.None,
+    )
+}
