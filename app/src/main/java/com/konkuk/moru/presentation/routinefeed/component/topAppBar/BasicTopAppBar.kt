2b07ef1f557a8f1ac7e1e9c709fb8aa0a79d8f70
jhsoo0211 <jhsoo0211@naver.com>
2025-07-13 16:24:23 +0900

    [chore] 필요없는 import 수정

--- app/src/main/java/com/konkuk/moru/presentation/routinefeed/component/topAppBar/BasicTopAppBar.kt
+++ app/src/main/java/com/konkuk/moru/presentation/routinefeed/component/topAppBar/BasicTopAppBar.kt
@@ -6,9 +6,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
--- app/src/main/java/com/konkuk/moru/presentation/routinefeed/screen/follow/FollowScreen.kt
+++ app/src/main/java/com/konkuk/moru/presentation/routinefeed/screen/follow/FollowScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
