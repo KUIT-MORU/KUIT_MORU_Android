41c00029419b2a89433e6400bfdfd3c753476e9a
jhsoo0211 <jhsoo0211@naver.com>
2025-07-14 00:44:20 +0900

    [UI] 뒷배경 흰색으로 바꿈

--- app/src/main/java/com/konkuk/moru/presentation/routinefeed/data/RoutineFeedModels.kt
+++ app/src/main/java/com/konkuk/moru/presentation/routinefeed/data/RoutineFeedModels.kt
@@ -3,20 +3,10 @@ package com.konkuk.moru.presentation.routinefeed.data
 import androidx.annotation.DrawableRes
 import com.konkuk.moru.data.model.Routine
 
-data class RoutineInfo(
-    val id: Int,
-    val name: String,
-    val tags: List<String>,
-    val likes: Int,
-    val isRunning: Boolean,
-    var isLiked: Boolean
-)
-
 data class RoutineSectionModel(
     val title: String,
     val routines: List<Routine>
 )
-// 전체 Routine으로 수정해놈
 
 data class LiveUserInfo(
     val id: Int,
