6547a79648540e90d5afb7bd1646f6a3b39450a2
hyungyu-02 <yona20020209@gmail.com>
2025-07-14 16:24:59 +0900

    feat: 회원가입 화면 제작

--- app/src/main/java/com/konkuk/moru/presentation/signup/SignUpScreen.kt
+++ app/src/main/java/com/konkuk/moru/presentation/signup/SignUpScreen.kt
@@ -1,5 +1,7 @@
 package com.konkuk.moru.presentation.signup
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -7,11 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,53 +19,124 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.konkuk.moru.presentation.signup.component.SignUpTextField
+import com.konkuk.moru.presentation.signup.component.TopBarLogoWithTitle
+import com.konkuk.moru.ui.theme.MORUTheme.colors
+import com.konkuk.moru.ui.theme.MORUTheme.typography
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 
 @Composable
 fun SignUpScreen(navController: NavController) {
+    val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var nickname by remember { mutableStateOf("") }
+
+    val isEmailValid = remember(email) {
+        android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+    val isPasswordValid = remember(password) {
+        password.isNotBlank()
+    }
+    //val isFormValid = isEmailValid && isPasswordValid
+    val isFormValid = true // 임시로 유효성 검사 생략
+
+    val focusManager = LocalFocusManager.current
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp),
-        verticalArrangement = Arrangement.Center
+            .background(color = colors.charcoalBlack)
+            .systemBarsPadding()
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onTap = {
+                        focusManager.clearFocus() // 화면을 탭하면 포커스를 해제
+                    }
+                )
+            }
     ) {
-        Text("회원가입", style = MaterialTheme.typography.headlineMedium)
-        Spacer(modifier = Modifier.height(16.dp))
-
-        TextField(value = email, onValueChange = { email = it }, label = { Text("이메일") }, modifier = Modifier.fillMaxWidth())
-        Spacer(modifier = Modifier.height(8.dp))
-
-        TextField(value = password, onValueChange = { password = it }, label = { Text("비밀번호") }, visualTransformation = PasswordVisualTransformation(), modifier = Modifier.fillMaxWidth())
-        Spacer(modifier = Modifier.height(8.dp))
-
-        TextField(value = nickname, onValueChange = { nickname = it }, label = { Text("닉네임") }, modifier = Modifier.fillMaxWidth())
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = {
-                navController.navigate("main") {
-                    popUpTo("login") { inclusive = true } // 로그인 화면 뒤로가기 막기
-                }
-            },
-            modifier = Modifier.fillMaxWidth()
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+                .background(Color(0xFF1A1A1A)),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Text("가입하기")
+            TopBarLogoWithTitle()
         }
 
-        TextButton(
-            onClick = { navController.popBackStack() },
-            modifier = Modifier.align(Alignment.End)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color(0xFFFFFFFF))
+                .padding(horizontal = 32.dp),
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
-            Text("뒤로가기")
+            Column {
+                Spacer(modifier = Modifier.height(screenHeight * 0.05f))
+                Text(
+                    text = "회원가입",
+                    style = typography.body_SB_24
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Text(
+                    text = "이메일",
+                    style = typography.body_SB_16
+                )
+
+                Spacer(modifier = Modifier.height(6.dp))
+
+                SignUpTextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    placeholder = "이메일",
+                    keyboardType = KeyboardType.Email
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(
+                    text = "비밀번호",
+                    style = typography.body_SB_16
+                )
+
+                Spacer(modifier = Modifier.height(6.dp))
+
+                SignUpTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    placeholder = "비밀번호",
+                    keyboardType = KeyboardType.Password,
+                    visualTransformation = PasswordVisualTransformation()
+                )
+            }
+
+            Button(
+                onClick = {
+                    navController.navigate("main") {
+                        popUpTo("login") { inclusive = true }
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp),
+                enabled = isFormValid // ✅ 여기!
+            ) {
+                Text("가입하기")
+            }
         }
     }
 }
---
+++ app/src/main/java/com/konkuk/moru/presentation/signup/component/SignUpTextField.kt
@@ -0,0 +1,94 @@
+package com.konkuk.moru.presentation.signup.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.konkuk.moru.presentation.login.component.LoginTextFieldBasic
+import com.konkuk.moru.ui.theme.MORUTheme.colors
+import com.konkuk.moru.ui.theme.MORUTheme.typography
+
+@Composable
+fun SignUpTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholder: String,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+
+    val borderColor = if (isFocused) colors.darkGray else colors.lightGray
+
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChange,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+        visualTransformation = visualTransformation,
+        interactionSource = interactionSource,
+        textStyle = typography.desc_M_14,
+        cursorBrush = SolidColor(colors.darkGray),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(45.dp)
+            .border(
+                width = 1.dp,
+                color = borderColor,
+                shape = RoundedCornerShape(4.dp)
+            )
+            .background(color = Color.Transparent),
+        decorationBox = { innerTextField ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                if (value.isEmpty()) {
+                    Text(
+                        text = placeholder,
+                        style = typography.desc_M_14,
+                        color = if (isFocused) colors.darkGray else colors.mediumGray,
+                    )
+                }
+                innerTextField()
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BasicPreview() {
+    SignUpTextField(
+        value = "",
+        onValueChange = {},
+        placeholder = "이메일",
+        keyboardType = KeyboardType.Email,
+        visualTransformation = VisualTransformation.None,
+    )
+}
