9cf214ecd5144716ed88554f3ece46a64eaaf982
hyungyu-02 <yona20020209@gmail.com>
2025-07-19 01:10:23 +0900

    feat: 회원가입 완료 시 팝업 구현

--- app/src/main/java/com/konkuk/moru/presentation/signup/SignUpScreen.kt
+++ app/src/main/java/com/konkuk/moru/presentation/signup/SignUpScreen.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,11 +15,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
@@ -37,7 +38,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.konkuk.moru.presentation.navigation.Route
+import com.konkuk.moru.presentation.signup.component.CompleteSignupPopup
 import com.konkuk.moru.presentation.signup.component.SignUpButton
+import kotlinx.coroutines.delay
 
 @Composable
 fun SignUpScreen(
@@ -60,149 +63,134 @@ fun SignUpScreen(
 
     var emailInvalidMessage by remember { mutableStateOf<String?>(null) }
 
-    Column(
+    var showPopup by remember { mutableStateOf(false) }
+    LaunchedEffect(showPopup) {
+        if (showPopup) {
+            delay(3000)
+            navController.navigate(Route.AuthCheck.route) {
+                popUpTo(Route.SignUp.route) { inclusive = true }
+            }
+        }
+    }
+
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .background(color = colors.charcoalBlack)
             .systemBarsPadding()
             .pointerInput(Unit) {
                 detectTapGestures(
-                    onTap = {
-                        focusManager.clearFocus() // 화면을 탭하면 포커스를 해제
-                    }
+                    onTap = { focusManager.clearFocus() }
                 )
             }
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(56.dp)
-                .background(Color(0xFF1A1A1A)),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
+        Column(modifier = Modifier.fillMaxSize()) {
             TopBarLogoWithTitle()
-        }
 
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-                .background(Color(0xFFFFFFFF))
-                .padding(horizontal = 32.dp)
-                .padding(bottom = 10.dp),
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            Column {
-                Spacer(modifier = Modifier.height(screenHeight * 0.05f))
-                Text(
-                    text = "회원가입",
-                    style = typography.body_SB_24
-                )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .background(Color(0xFFFFFFFF))
+                    .padding(horizontal = 32.dp)
+                    .padding(bottom = 10.dp),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column {
+                    Spacer(modifier = Modifier.height(screenHeight * 0.05f))
 
-                Spacer(modifier = Modifier.height(24.dp))
+                    Text(text = "회원가입", style = typography.body_SB_24)
 
-                Text(
-                    text = "이메일",
-                    style = typography.body_SB_16
-                )
+                    Spacer(modifier = Modifier.height(24.dp))
 
-                Spacer(modifier = Modifier.height(6.dp))
+                    Text(text = "이메일", style = typography.body_SB_16)
 
-                SignUpTextField(
-                    value = email,
-                    onValueChange = {
-                        email = it
-                        isEmailValid = android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
-                        emailInvalidMessage =
-                            if (email.isNotBlank() && !isEmailValid) {
-                                "이메일 형식이 올바르지 않습니다."
-                            } else {
-                                null
-                            }
-                    },
-                    isValid = email.isEmpty() || isEmailValid,
-                    placeholder = "이메일",
-                    keyboardType = KeyboardType.Email
-                )
+                    Spacer(modifier = Modifier.height(6.dp))
 
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(24.dp)
+                    SignUpTextField(
+                        value = email,
+                        onValueChange = {
+                            email = it
+                            isEmailValid = android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+                            emailInvalidMessage =
+                                if (email.isNotBlank() && !isEmailValid) { "이메일 형식이 올바르지 않습니다." }
+                                else { null }
+                        },
+                        isValid = email.isEmpty() || isEmailValid,
+                        placeholder = "이메일",
+                        keyboardType = KeyboardType.Email
+                    )
+
+                    Row(modifier = Modifier.fillMaxWidth().height(24.dp)) {
+                        emailInvalidMessage?.let {
+                            Text(
+                                text = it,
+                                style = typography.desc_M_12,
+                                color = colors.red,
+                                modifier = Modifier.padding(top = 6.dp, start = 4.dp)
+                            )
+                        }
+                    }
+
+                    Text(text = "비밀번호", style = typography.body_SB_16)
+
+                    Spacer(modifier = Modifier.height(6.dp))
+
+                    SignUpTextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        isValid = password.isEmpty() || isPasswordValid,
+                        placeholder = "비밀번호",
+                        keyboardType = KeyboardType.Password,
+                        visualTransformation = PasswordVisualTransformation()
+                    )
+
+                    Spacer(modifier = Modifier.height(6.dp))
+
+                    Text(
+                        text = "8자 이상, 숫자 특수문자를 포함해주세요.",
+                        style = typography.desc_M_12,
+                        color =
+                            if (password.isNotBlank() && !isPasswordValid) { colors.red }
+                            else { colors.mediumGray },
+                        modifier = Modifier.padding(start = 4.dp)
+                    )
+                }
+
+                SignUpButton(
+                    enabled = isFormValid
                 ) {
-                    emailInvalidMessage?.let {
-                        Text(
-                            text = it,
-                            style = typography.desc_M_12,
-                            color = colors.red,
-                            modifier = Modifier.padding(top = 6.dp, start = 4.dp)
+                    if (isFormValid) {
+                        viewModel.signUp(
+                            email, password, context,
+                            onSuccess = {
+                                showPopup = true
+                            },
+                            onFailure = { error ->
+                                // TODO: 에러 메시지 UI에 띄우기
+                                Log.e("SignUpScreen", "회원가입 실패: $error")
+                            }
                         )
+                    } else {
+                        // 유효성 검사 실패 시 처리
+                        // ========= 비활성 버튼도 임시로 작동하도록 설정. 추후 기능 제거 필요 =====
+                        viewModel.signUp(
+                            email, password, context,
+                            onSuccess = {
+                                showPopup = true
+                            },
+                            onFailure = { error ->
+                                // TODO: 에러 메시지 UI에 띄우기
+                                Log.e("SignUpScreen", "회원가입 실패: $error")
+                            }
+                        )
+                        // ============================================================
                     }
                 }
-
-                Text(
-                    text = "비밀번호",
-                    style = typography.body_SB_16
-                )
-
-                Spacer(modifier = Modifier.height(6.dp))
-
-                SignUpTextField(
-                    value = password,
-                    onValueChange = { password = it },
-                    isValid = password.isEmpty() || isPasswordValid,
-                    placeholder = "비밀번호",
-                    keyboardType = KeyboardType.Password,
-                    visualTransformation = PasswordVisualTransformation()
-                )
-
-                Spacer(modifier = Modifier.height(6.dp))
-
-                Text(
-                    text = "8자 이상, 숫자 특수문자를 포함해주세요.",
-                    style = typography.desc_M_12,
-                    color = if (password.isNotBlank() && !isPasswordValid) {
-                        colors.red
-                    } else {
-                        colors.mediumGray
-                    },
-                    modifier = Modifier.padding(start = 4.dp)
-                )
             }
-
-            SignUpButton(
-                enabled = isFormValid
-            ) {
-                if (isFormValid) {
-                    viewModel.signUp(email, password, context,
-                        onSuccess = {
-                            navController.navigate(Route.AuthCheck.route) {
-                                popUpTo(Route.SignUp.route) { inclusive = true }
-                            }
-                        },
-                        onFailure = { error ->
-                            // TODO: 에러 메시지 UI에 띄우기
-                            Log.e("SignUpScreen", "회원가입 실패: $error")
-                        }
-                    )
-                } else {
-                    // 유효성 검사 실패 시 처리
-                    // ========= 비활성 버튼도 임시로 작동하도록 설정. 추후 기능 제거 필요 =====
-                    viewModel.signUp(email, password, context,
-                        onSuccess = {
-                            navController.navigate(Route.AuthCheck.route) {
-                                popUpTo(Route.SignUp.route) { inclusive = true }
-                            }
-                        },
-                        onFailure = { error ->
-                            // TODO: 에러 메시지 UI에 띄우기
-                            Log.e("SignUpScreen", "회원가입 실패: $error")
-                        }
-                    )
-                    // ============================================================
-                }
-            }
+        }
+        if (showPopup) {
+            CompleteSignupPopup()
         }
     }
 }
--- app/src/main/java/com/konkuk/moru/presentation/signup/component/TopBarLogoWithTitle.kt
+++ app/src/main/java/com/konkuk/moru/presentation/signup/component/TopBarLogoWithTitle.kt
@@ -1,8 +1,12 @@
 package com.konkuk.moru.presentation.signup.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -10,35 +14,44 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.konkuk.moru.R
 import com.konkuk.moru.ui.theme.MORUTheme.colors
 import com.konkuk.moru.ui.theme.MORUTheme.typography
 
 @Composable
 fun TopBarLogoWithTitle() {
-    Row(
-        verticalAlignment = Alignment.CenterVertically
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .background(Color(0xFF1A1A1A)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_signup_moru_logo),
-            contentDescription = "Moru Logo",
-            modifier = Modifier
-                .padding(0.dp)
-                .width(30.dp)
-                .height(30.dp)
-        )
-        Spacer(modifier = Modifier.width(3.dp))
-        Text(
-            text = "MORU",
-            style = typography.desc_M_20,
-            color = colors.paleLime
-        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_signup_moru_logo),
+                contentDescription = "Moru Logo",
+                modifier = Modifier
+                    .padding(0.dp)
+                    .width(30.dp)
+                    .height(30.dp)
+            )
+            Spacer(modifier = Modifier.width(3.dp))
+            Text(
+                text = "MORU",
+                style = typography.desc_M_20,
+                color = colors.paleLime
+            )
+        }
     }
+
 }
 
 @Preview
