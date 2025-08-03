64801bfd5cdc5136ce042fb568153a1747942410
adagioCHAN <gguakchan@gmail.com>
2025-08-03 20:39:54 +0900

    [refactor] bottom bar로 인한 padding 오류 수정

--- app/src/main/java/com/konkuk/moru/presentation/navigation/AppNavGraph.kt
+++ app/src/main/java/com/konkuk/moru/presentation/navigation/AppNavGraph.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -148,7 +150,7 @@ fun AppNavGraph(
                         )
                     ) {
                         MoruBottomBar(
-                            modifier = Modifier.height(80.dp),
+                            modifier = Modifier.wrapContentHeight().navigationBarsPadding(),
                             selectedRoute = currentRoute ?: Route.Home.route,
                             onItemSelected = { route ->
                                 if (currentRoute != route) {
