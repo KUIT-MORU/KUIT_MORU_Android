c27dfaf5684554fb7fad52d7a52cfd402812470f
adagioCHAN <gguakchan@gmail.com>
2025-08-21 12:09:30 +0900

    [refactor] 스크랩한 루틴 디자인 수정

--- app/src/main/java/com/konkuk/moru/presentation/myactivity/component/ScrabRoutine.kt
+++ app/src/main/java/com/konkuk/moru/presentation/myactivity/component/ScrabRoutine.kt
@@ -82,7 +82,7 @@ fun ScrabRoutine(
         )
 
         Text(
-            text = tags.joinToString(" ") { "#$it" },
+            text = tags,
             color = colors.darkGray,
             style = typography.time_R_12,
             maxLines = 1,
